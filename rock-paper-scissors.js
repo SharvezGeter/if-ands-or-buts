5232951007a74d1175d6cbe72c74472d9364ee3c
SharvezGeter <sharvezgeter@gmail.com>
2022-09-19 16:09:25 -0400

    updated

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -3,37 +3,20 @@ const prompt = require('prompt-sync')({sigint: true});
 let player1 = (prompt("Player1 Enter Rock, Paper, or Scissors."));
 let player2 = (prompt("Player2 Enter Rock, Paper, or Scissors."));
 
-
 console.log(player1)
 console.log(player2)
 
-
-
-
 if (player1 === player2){
     console.log("Draw")
-}
-if(player1 === "rock" && player2 === "paper")
+}else if(player1 === "rock" && player2 === "paper"){
     console.log("Player2 Wins")
-
-if(player1 === "paper" && player2 === "rock")
+}else if(player1 === "paper" && player2 === "rock"){
     console.log("Player1 Wins")
-
- if (player1 === "rock" && player2 === "scissors")
+}else if (player1 === "rock" && player2 === "scissors"){
     console.log("Player1 Wins")
-
- if(player1 === "scissors" && player2 === "rock")
+ }else if(player1 === "scissors" && player2 === "rock"){
     console.log("Player2 Wins")
-
-if (player1 === "scissors" && player2 === "paper")
+ }else if (player1 === "scissors" && player2 === "paper"){
     console.log("Player2 Wins")
-
-//  else if player1 !== "rock" , "paper", "scissors" && player2 !== "rock" , "paper", "scissors"{
-//     console.log("error")
-// }
-//  else if (player2 !== "rock" , "paper", "scissors");{
-//     console.log("error")
-// }
-
-else if (player1 || player2 !== "scissors", "paper", "rock"){
+}else if (player1 || player2 !== "scissors", "paper", "rock"){
     console.log("Error")}
